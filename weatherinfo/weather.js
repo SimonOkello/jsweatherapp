f30342d6695e86608cd500fc2fa4feec4f4db31d
SimonOkello <simonokello93@gmail.com>
2020-05-28 20:46:15 +0300

    feature(weather): consume OpenWeather API to get weather of the city

--- weatherinfo/weather.js
+++ weatherinfo/weather.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const getWeatherInfo = async ( lat, lng ) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ latitude }&lon=${ longitude }&appid=32f843d833c38373032f825c4a92418a&units=metric`)
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=32f843d833c38373032f825c4a92418a&units=metric`)
 
     return resp.data.main.temp;
 
